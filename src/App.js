323dae5b85709e45fb4ffa5cf13d2f17eb6da938
Glen1986 <escobar.glen@gmail.com>
2022-05-06 22:38:28 -0300

    react-redux-add-todo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 const initialState = {
     entities: [],
@@ -5,23 +6,61 @@ const initialState = {
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'todo/add': {
-            console.log('reducer')
             return {
                 ...state,
-                entities: [{}],
+                entities: state.entities.concat({ ...action.payload }),
             }
         }
+        case 'todo/completed': {
+            const newTodos = state.entities.map((todo) => {
+                if (todo.id === action.payload.id) {
+                    return { ...todo, completed: !todo.completed }
+                }
+                return todo
+            })
+            return {
+                ...state,
+                entities: newTodos,
+            }
+        }
+        default:
+            return state
     }
-    return state
+}
+const TodoItem = ({ todo }) => {
+    const dispatch = useDispatch()
+    return (
+        <li
+            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+            onClick={() => dispatch({ type: 'todo/completed', payload: todo })}
+        >
+            {todo.title}
+        </li>
+    )
 }
 const App = () => {
+    const [value, setValue] = useState()
     const dispatch = useDispatch()
     const state = useSelector((x) => x)
-    console.log(state, 'rendering')
+
+    const submit = (e) => {
+        e.preventDefault()
+        if (!value.trim()) {
+            return
+        }
+        const id = Math.random().toString(36)
+        const todo = { title: value, completed: false, id }
+        dispatch({ type: 'todo/add', payload: todo })
+        setValue('')
+    }
+
     return (
         <div>
-            <form>
-                <input type="text" />
+            <form onSubmit={submit}>
+                <input
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                />
             </form>
             <button onClick={() => dispatch({ type: 'todo/add' })}>
                 todos
@@ -29,8 +68,9 @@ const App = () => {
             <button>completados</button>
             <button>incompletos</button>
             <ul>
-                <li>todo1</li>
-                <li>todo2</li>
+                {state.entities.map((todo) => (
+                    <TodoItem key={todo.id} todo={todo} />
+                ))}
             </ul>
         </div>
     )
